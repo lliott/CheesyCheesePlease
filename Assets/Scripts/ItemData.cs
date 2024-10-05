30a529f04732281ff722b0a82a17e244c641de97
pikaillott <40119956+pikaillott@users.noreply.github.com>
2024-10-05 13:45:26 +0200

    aaa

--- Assets/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Item/ItemData")]
-public class ItemData : ScriptableObject
-{
-    public string itemName;
-    public int itemID;
-    public GameObject itemPrefab;
-}
