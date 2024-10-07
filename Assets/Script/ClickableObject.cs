3f4ec55ecf9b6739836299a34e48b2209b405414
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2024-10-07 20:24:15 +0200

    [ADD]Audio

--- Assets/Script/ClickableObject.cs
+++ Assets/Script/ClickableObject.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class ClickableObject : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private Temperature _tempScript;
     [SerializeField] private GameObject objectToDisplay;
     [SerializeField] private float _displayTime;
     private float _remainingTime ;
     private bool _isDisplaying = false;
+    private AudioSource _audio;
+
 
     void Awake()
     {
-        objectToDisplay.SetActive(false); 
+        objectToDisplay.SetActive(false);
         ResetDialogue() ;
-    }   
+       
+    }  
+
+
+    void Start() {
+        if(gameObject.GetComponent<AudioSource>() != null)
+        {
+            _audio = GetComponent<AudioSource>();
+            //_audio.enabled = false;
+        }
+    }
+
 
     void Update(){
         if(_displayTime != 0){
@@ -26,24 +41,35 @@ public class ClickableObject : MonoBehaviour, IPointerClickHandler
         }
     }
 
+
     void ResetDialogue(){
         _remainingTime = _displayTime;
     }
+
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if(objectToDisplay.name == "dialogue"){
             Debug.Log("objet à display: dialogue");
             PassengerInfoController.instance.GiveGift();
+            _tempScript.GetScanner();// Monter le scanner
         }
 
+
         _isDisplaying = true ;
+
 
         if (!objectToDisplay.activeSelf)
         {
             objectToDisplay.SetActive(true);
+            if(_audio!=null){
+                _audio.Play();
+            }
         }
 
-        
+
+       
     }
 }
+
+
--- Assets/Script/PASS UI/CloseableObject.cs
+++ Assets/Script/PASS UI/CloseableObject.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class CloseableObject : MonoBehaviour, IPointerClickHandler
 {
+    private AudioSource _audio;
+
+
+    void Start() {
+        if(gameObject.GetComponent<AudioSource>() != null)
+        {
+            _audio = GetComponent<AudioSource>();
+            //_audio.enabled = false;
+        }
+    }
+
+
     public void OnPointerClick(PointerEventData eventData)
+    {
+        if(_audio != null){
+            _audio.Play();
+        }
+        Invoke("DeactivateGameObject", 0.25f);
+    }
+
+
+    void DeactivateGameObject()
     {
         gameObject.SetActive(false);
     }
