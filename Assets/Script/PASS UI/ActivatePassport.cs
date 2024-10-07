f23e3f006853bebb5aaf56288b88a19462acfc96
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2024-10-07 20:54:37 +0200

    [ADD]Audio

--- Assets/Script/PASS UI/ActivatePassport.cs
+++ Assets/Script/PASS UI/ActivatePassport.cs
@@ -2,24 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ActivatePassport : MonoBehaviour
 {
     public GameObject uiElement;
     public GameObject passportButton;
+    private AudioSource _audio;
+
+
+    void Start(){
+        _audio = GetComponent<AudioSource>();
+    }
+
 
     public void ActivateUI()
     {
         uiElement.SetActive(true);
         passportButton.SetActive(false);
+        _audio.Play();
     }
+
 
     public void DeactivateUI()
     {
         uiElement.SetActive(false);
     }
 
+
     public void ToggleUI()
     {
         uiElement.SetActive(!uiElement.activeSelf);
     }
 }
+
+
+
--- Assets/Script/PASS UI/PASSPORTCloseableObject.cs
+++ Assets/Script/PASS UI/PASSPORTCloseableObject.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class PASSPORTCloseableObject : MonoBehaviour, IPointerClickHandler
 {
 
+
     [SerializeField] GameObject passport;
+    private AudioSource _audio;
+
+
+    void Start() {
+        if(gameObject.GetComponent<AudioSource>() != null)
+        {
+            _audio = GetComponent<AudioSource>();
+            //_audio.enabled = false;
+        }
+    }
+
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        gameObject.SetActive(false);
+        if(_audio != null){
+            _audio.Play();
+        }
         passport.SetActive(true);
+       
+        Invoke("DeactivateGameObject", 0.25f);
+    }
+    void DeactivateGameObject()
+    {
+        gameObject.SetActive(false);
     }
 }
